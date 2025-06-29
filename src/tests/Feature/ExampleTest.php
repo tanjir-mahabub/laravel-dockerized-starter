904bb74b16dad7eed12cefbad63a98e4760549a7
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2025-06-29 19:45:07 +0600

    Fix Intelephense warnings in Laravel tests - add PHPDoc annotations to suppress false positives

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -5,13 +5,20 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * @see \Illuminate\Foundation\Testing\Concerns\MakesHttpRequests
+ * @method \Illuminate\Testing\TestResponse get(string $uri, array $headers = [])
+ */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
+     * 
+     * @return void
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        /** @var \Illuminate\Testing\TestResponse $response */
         $response = $this->get('/');
 
         $response->assertStatus(200);
--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -4,6 +4,11 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * @extends BaseTestCase
+ * @phpstan-ignore-next-line
+ */
+// @phpstan-ignore-next-line
 abstract class TestCase extends BaseTestCase
 {
     //
